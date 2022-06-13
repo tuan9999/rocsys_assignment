416b212917d8be2e808f8e9e090a18e906871ce6
tuan9999 <tuanperera@gmail.com>
2022-06-13 12:27:36 +0200

    Move fns to be methods on struct

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,8 @@
 use clap::Parser;
-use rocsys::models::Input;
-use notify::{Watcher, RecursiveMode, watcher, DebouncedEvent::{*, self}};
-use std::{sync::mpsc::channel};
-use std::time::Duration;
-
-fn handle_event(event: DebouncedEvent) {
-    match event {
-        NoticeWrite(path_buf) => { println!("Event: Notice Write to {:?}", path_buf) },
-        NoticeRemove(path_buf) => { println!("Event: Notice Remove {:?}", path_buf) },
-        Create(path_buf) => { println!("Event: Create {:?}", path_buf) },
-        Write(path_buf) => { println!("Event: Write to {:?}", path_buf) },
-        Chmod(path_buf) => { println!("Event: Chmod on {:?}", path_buf) },
-        Remove(path_buf) => { println!("Event: Remove {:?}", path_buf) },
-        Rename(path_buf_before, path_buf_after) => { println!("Event: Rename {:?} to {:?}", path_buf_before, path_buf_after) },
-        Rescan => { println!("Event: Rescan") },
-        Error(error, path_buf) => {
-            match path_buf {
-                Some(path) => println!("Error: {}, PATH = {:?}", error, path),
-                None => println!("Error: {}", error)
-            }
-        },
-    }
-}
-
-fn monitor_directories(input: &Input) {
-    // Create a channel to receive the events.
-    let (tx, rx) = channel();
-    
-    // Create a watcher object, delivering debounced events.
-    // The notification back-end is selected based on the platform.
-    let mut watcher = watcher(tx, Duration::from_secs(0)).unwrap();
-    
-    // Add a path to be watched. All files and directories at that path and
-    // below will be monitored for changes.
-    for directory in &input.directories {
-        watcher.watch(directory.clone(), RecursiveMode::Recursive).unwrap();
-    }
-    
-    loop {
-        match rx.recv() {
-           Ok(event) => handle_event(event),
-           Err(e) => println!("watch error: {:?}", e),
-        }
-    }
-}
+use rocsys::models::{DirectoryMonitor, Input};
 
 fn main() {
     let input = Input::parse();
-    println!("{:?}", input.directories);
-    monitor_directories(&input)
+    let mut directory_monitor = DirectoryMonitor::new(input);
+    directory_monitor.monitor_directories();
 }
--- src/models.rs
+++ src/models.rs
@@ -1,4 +1,11 @@
 use clap::Parser;
+use notify::{
+    watcher,
+    DebouncedEvent::{self, *},
+    RecursiveMode, Watcher,
+};
+use std::sync::mpsc::channel;
+use std::time::Duration;
 
 #[derive(Parser, Debug, Clone)]
 #[clap(author, version, about, long_about = None)]
@@ -8,5 +15,66 @@ pub struct Input {
 }
 
 pub struct DirectoryMonitor {
+    input: Input,
+}
 
+impl DirectoryMonitor {
+    pub fn new(input: Input) -> Self {
+        Self { input }
+    }
+
+    fn handle_event(&mut self, event: DebouncedEvent) {
+        match event {
+            NoticeWrite(path_buf) => {
+                println!("Event: Notice Write to {:?}", path_buf)
+            }
+            NoticeRemove(path_buf) => {
+                println!("Event: Notice Remove {:?}", path_buf)
+            }
+            Create(path_buf) => {
+                println!("Event: Create {:?}", path_buf)
+            }
+            Write(path_buf) => {
+                println!("Event: Write to {:?}", path_buf)
+            }
+            Chmod(path_buf) => {
+                println!("Event: Chmod on {:?}", path_buf)
+            }
+            Remove(path_buf) => {
+                println!("Event: Remove {:?}", path_buf)
+            }
+            Rename(path_buf_before, path_buf_after) => {
+                println!(
+                    "Event: Rename {:?} to {:?}",
+                    path_buf_before, path_buf_after
+                )
+            }
+            Rescan => {
+                println!("Event: Rescan")
+            }
+            Error(error, path_buf) => match path_buf {
+                Some(path) => println!("Error: {}, PATH = {:?}", error, path),
+                None => println!("Error: {}", error),
+            },
+        }
+    }
+
+    pub fn monitor_directories(&mut self) {
+        let (tx, rx) = channel();
+
+        let mut watcher = watcher(tx, Duration::from_secs(0)).unwrap();
+
+        for directory in &self.input.directories {
+            watcher
+                .watch(directory.clone(), RecursiveMode::Recursive)
+                .unwrap();
+        }
+
+        loop {
+            match rx.recv() {
+                Ok(event) => self.handle_event(event),
+                Err(e) => println!("watch error: {:?}", e),
+            }
+        }
+    }
 }
